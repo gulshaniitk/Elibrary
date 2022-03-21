288b296f3a8b964d9079b66031402abe343e3989
rgoyal12 <85826885+rgoyal12@users.noreply.github.com>
2022-03-21 15:43:49 +0530

    Add files via upload

---
+++ elibrary/index.php
@@ -0,0 +1,43 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    
+  </head>
+  <body>
+    
+
+
+
+    <div class = "container">
+  <div class ="row">
+      <div class = "col">
+          Column
+      </div>
+      <div class = "col">
+          Column
+      </div>
+      <div class = "col">
+          Column
+      </div>
+      <div class = "col">
+          Column
+      </div>
+
+
+  </div>
+
+</div>
+<button type="button" class="btn btn-primary btn-sm">Large button</button>
+<button type="button" class="btn btn-secondary btn-lg">Large button</button>
+  </body>
+
+
+
+</html>
