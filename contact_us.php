b9939afb480496cdb7cfbdc57b34140cc2aff9c0
gulshaniitk <gulshan@iitk.ac.in>
2022-03-27 16:27:27 +0530

    demo commit

--- contact_us.php
+++ contact_us.php
@@ -1,3 +1,4 @@
+
 <!doctype html>
 <html lang="en">
   <head>
@@ -9,6 +10,15 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
     <title>User Home</title>
+    <style>
+ body{
+  background: rgb(34,193,195);
+background: linear-gradient(0deg, rgba(34,193,195,1) 0%, rgba(253,187,45,1) 100%);
+background-size: 100% 100%;
+background-repeat: no-repeat;
+background-attachment:fixed ;
+ }
+    </style>
   </head>
 
   
