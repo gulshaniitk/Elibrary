f917361948339298d66dac5e6f639168349e1f28
gulshaniitk <gulshan@iitk.ac.in>
2022-03-30 12:25:49 +0530

    gulshan commit

--- issuebookdelete.php
+++ issuebookdelete.php
@@ -15,9 +15,22 @@ $bi=$_GET['bi'];
 
 $sql="delete from issuebook where rollno=$rn and bookid=$bi";
 $res=mysqli_query($conn,$sql);
+$q1="SELECT `Quantity` FROM `books` WHERE Bookid='$bi'";
+
+$quant=mysqli_query($conn,$q1);
+
+$curr = mysqli_fetch_assoc($quant);
+
+$quantity=$curr['Quantity'];
+$quantity=$quantity+1;
+
+
+$q2="UPDATE `books` SET `Quantity`='$quantity' WHERE Bookid='$bookid'";
+
+$quant2=mysqli_query($conn,$q2);
 
 if ($res) {
-    echo "<script>alert('Deleted successfully');window.location.href='issuebook.php?rn=$rn';</script>";
+    echo "<script>alert('Book returned successfully');window.location.href='issuebook.php?rn=$rn';</script>";
     
   } else {
     echo "Error: " . $sql . "<br>" . mysqli_error($conn);
