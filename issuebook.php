e3547eca9bab1b1fdcc3fe9499c63c37f9e264ff
gulshaniitk <gulshan@iitk.ac.in>
2022-04-11 17:09:50 +0530

    gulshan commit

--- issuebook.php
+++ issuebook.php
@@ -232,7 +232,7 @@ while($res=mysqli_fetch_array($table))
 <div class="container">
         <div class="row">
             <div class="col-lg-7">
-            <h1 class="text-center">Issue Books</h1>
+            <h1>Issue Books</h1>
 </div>
 
 <div class="col-lg-5">
